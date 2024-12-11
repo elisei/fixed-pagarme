dfb6f958ad9b73b8084e10288001720fe6c8d05d
Elisei <brunoelisei@o2ti.com>
2024-12-11 08:28:10 -0300

    O2TI 😍 Magento - Corrige valores para Data de Expiração

--- Plugin/Checkout/Model/CompositeConfigProviderPlugin.php
+++ Plugin/Checkout/Model/CompositeConfigProviderPlugin.php
@@ -17,13 +17,67 @@ class CompositeConfigProviderPlugin
     public function afterGetConfig(CompositeConfigProvider $subject, array $result)
     {
         if (isset($result['payment']['ccform']['availableTypes']['pagbank_paymentmagento_cc'])) {
-            $types = $result['payment']['ccform']['availableTypes']['pagbank_paymentmagento_cc'];
+            $typesPagBank = isset($result['payment']['ccform']['availableTypes']['pagbank_paymentmagento_cc']) ?
+                $result['payment']['ccform']['availableTypes']['pagbank_paymentmagento_cc'] : null;
+            $typesGetnet = isset($result['payment']['ccform']['availableTypes']['getnet_paymentmagento_cc']) ?
+                $result['payment']['ccform']['availableTypes']['getnet_paymentmagento_cc'] : null;
 
-            foreach ($types as $cardType => $values) {
-                if (is_array($values)) {
-                    $result['payment']['ccform']['availableTypes']['pagbank_paymentmagento_cc'][$cardType] = 
-                        array_values(array_unique($values));
+            if ($typesPagBank) {
+                foreach ($typesPagBank as $cardType => $values) {
+                    if (is_array($values)) {
+                        $result['payment']['ccform']['availableTypes']['pagbank_paymentmagento_cc'][$cardType] =
+                            array_values(array_unique($values));
+                    }
                 }
+
+                $months = $result['payment']['ccform']['months']['pagbank_paymentmagento_cc'];
+
+                foreach ($months as $key => $month) {
+                    if ($key > 12) {
+                        unset($months[$key]);
+                    }
+                }
+
+                $result['payment']['ccform']['months']['pagbank_paymentmagento_cc'] = $months;
+
+                $years = $result['payment']['ccform']['years']['pagbank_paymentmagento_cc'];
+
+                foreach ($years as $key => $year) {
+                    if ($key != $year) {
+                        unset($years[$key]);
+                    }
+                }
+
+                $result['payment']['ccform']['years']['pagbank_paymentmagento_cc'] = $years;
+            }
+
+            if ($typesGetnet) {
+                foreach ($typesGetnet as $cardType => $values) {
+                    if (is_array($values)) {
+                            $result['payment']['ccform']['availableTypes']['getnet_paymentmagento_cc'][$cardType] =
+                            array_values(array_unique($values));
+                    }
+                }
+
+                $months = $result['payment']['ccform']['months']['getnet_paymentmagento_cc'];
+
+                foreach ($months as $key => $month) {
+                    if ($key > 12) {
+                        unset($months[$key]);
+                    }
+                }
+
+                $result['payment']['ccform']['months']['getnet_paymentmagento_cc'] = $months;
+
+                $years = $result['payment']['ccform']['years']['getnet_paymentmagento_cc'];
+
+                foreach ($years as $key => $year) {
+                    if ($key != $year) {
+                        unset($years[$key]);
+                    }
+                }
+
+                $result['payment']['ccform']['years']['getnet_paymentmagento_cc'] = $years;
             }
         }
         
